ea0c614df3e4ebb3c472aa9bcec6e9ed7af082c7
Andriy <55147571+BanyasAndriy@users.noreply.github.com>
2019-11-23 00:59:11 +0200

    Update Task2.java

--- Task2.java
+++ Task2.java
@@ -3,12 +3,6 @@ package HomeWork11.Stream_API;
 import java.util.*;
 import java.util.stream.IntStream;
 
-/*
- * 
- * 
- * 2. Напишите метод который найдет в массиве целых чисел число модуль которого ближе всего к 0.
- *  Если есть два таких числа (например 2 и -2) верните положительное.
- */
 public class Task2 {
 
 	public static void main(String[] args) {
